09beb1fc9daa284905dd7bdcd2c8a95e5154a43f
darusso <davide.russo83@gmail.com>
2016-04-04 15:29:12 +0200

    Added files via upload

---
+++ biblio_Russo/classes/Autore.php
@@ -0,0 +1,82 @@
+<?php
+
+class Autore 
+{
+    
+    private $id;
+    private $nome;
+    private $cognome;
+    private $data_nascita;
+    private $luogo_nascita;
+    private $vivente;  
+    
+    
+    function __construct($id, $nome, $cognome, $data_nascita, $luogo_nascita, $vivente) 
+    {
+        $this->id = $id;
+        $this->nome = $nome;
+        $this->cognome = $cognome;
+        $this->data_nascita = $data_nascita;
+        $this->luogo_nascita = $luogo_nascita;
+        $this->vivente = $vivente;
+    }
+    
+    function getId() {
+        return $this->id;
+    }
+
+    function getNome() {
+        return $this->nome;
+    }
+
+    function getCognome() {
+        return $this->cognome;
+    }
+
+    function getData_nascita() {
+        return $this->data_nascita;
+    }
+
+    function getLuogo_nascita() {
+        return $this->luogo_nascita;
+    }
+
+    function getVivente() {
+        return $this->vivente;
+    }
+
+    function setId($id) {
+        $this->id = $id;
+    }
+
+    function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    function setCognome($cognome) {
+        $this->cognome = $cognome;
+    }
+
+    function setData_nascita($data_nascita) {
+        $this->data_nascita = $data_nascita;
+    }
+
+    function setLuogo_nascita($luogo_nascita) {
+        $this->luogo_nascita = $luogo_nascita;
+    }
+
+    function setVivente($vivente) {
+        $this->vivente = $vivente;
+    }
+
+    public function __toString() 
+    {
+        
+    }
+
+
+
+    
+}
+
+
---
+++ biblio_Russo/classes/Editore.php
@@ -0,0 +1,53 @@
+<?php
+
+class Editore 
+{
+    
+    private $id;
+    private $nome;
+    private $citta;
+        
+    function __construct($id, $nome, $citta) {
+        $this->id = $id;
+        $this->nome = $nome;
+        $this->citta = $citta;
+    }
+    
+    function getId() {
+        return $this->id;
+    }
+
+    function getNome() {
+        return $this->nome;
+    }
+
+    function getCitta() {
+        return $this->citta;
+    }
+
+    function setId($id) {
+        $this->id = $id;
+    }
+
+    function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    function setCitta($citta) {
+        $this->citta = $citta;
+    }
+
+    public function __toString() {
+        
+    }
+
+
+    
+
+
+    
+    
+    
+}
+
+
